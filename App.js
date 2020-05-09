19b9aef0ac6a1ae5840abe921c442e6291a564e3
Sieusaosieungo <thinhhanh1@gmail.com>
2020-05-09 10:56:34 +0700

    guess game with react native

---
+++ App.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import * as Font from 'expo-font'; // expo-font is a package which gives you font utilities, so utilities that allow you to load fonts
+import { AppLoading } from 'expo';
+
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  }); // loadAsync allow to load font
+};
+
+export default function App() {
+  const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0); // the number of rounds it took to finish. if it's greater than 0, the game is over
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
+  const configureNewGameHandler = () => {
+    setGuessRounds(0);
+    setUserNumber(null);
+  };
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+    setGuessRounds(0);
+  };
+
+  const gameOverHandler = (numOfRounds) => {
+    setGuessRounds(numOfRounds);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber && guessRounds <= 0) {
+    content = (
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    );
+  } else if (guessRounds > 0) {
+    content = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onRestart={configureNewGameHandler}
+      />
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.screen}>
+      <Header title='Guess a Number' />
+      {content}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
---
+++ screens/GameOverScreen.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
+
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
+import colors from '../constants/colors';
+import MainButton from '../components/MainButton';
+
+const GameOverScreen = (props) => {
+  const [availableDeviceWidth, setAvailableDeviceWidth] = useState(
+    Dimensions.get('window').width
+  );
+  const [availableDeviceHeight, setAvailableDeviceHeight] = useState(
+    Dimensions.get('window').height
+  );
+
+  useEffect(() => {
+    const updateLayout = () => {
+      setAvailableDeviceWidth(Dimensions.get('window').width);
+      setAvailableDeviceHeight(Dimensions.get('window').height);
+    };
+
+    Dimensions.addEventListener('change', updateLayout);
+
+    return () => {
+      Dimensions.removeEventListener('change', updateLayout);
+    };
+  });
+
+  return (
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText>The Game is over</TitleText>
+        <View
+          style={{
+            ...styles.imageContainer,
+            ...{
+              width: availableDeviceWidth * 0.7,
+              height: availableDeviceWidth * 0.7,
+              borderRadius: (availableDeviceWidth * 0.7) / 2,
+              marginVertical: availableDeviceHeight / 30,
+            },
+          }}
+        >
+          <Image
+            fadeDuration={300}
+            source={require('../assets/success.png')}
+            style={styles.image}
+            resizeMode='cover'
+          />
+        </View>
+        <View
+          style={{
+            ...styles.resultContainer,
+            ...{ marginVertical: availableDeviceHeight / 60 },
+          }}
+        >
+          <BodyText
+            style={{
+              ...styles.resultText,
+              ...{ fontSize: availableDeviceHeight < 400 ? 16 : 20 },
+            }}
+          >
+            Your phone needed{' '}
+            <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+            guess the number{' '}
+            <Text style={styles.highlight}>{props.userNumber}</Text>
+          </BodyText>
+        </View>
+        <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  imageContainer: {
+    width: Dimensions.get('window').width * 0.7,
+    height: Dimensions.get('window').width * 0.7,
+    borderRadius: Dimensions.get('window').width * 0.35,
+    borderWidth: 3,
+    borderColor: 'black',
+    overflow: 'hidden',
+    marginVertical: Dimensions.get('window').height / 30,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  resultContainer: {
+    marginHorizontal: 30,
+    marginVertical: Dimensions.get('window').height / 60,
+  },
+  resultText: {
+    textAlign: 'center',
+    fontSize: Dimensions.get('window').height < 400 ? 16 : 20,
+  },
+  highlight: {
+    color: colors.primary,
+    fontFamily: 'open-sans-bold',
+  },
+});
